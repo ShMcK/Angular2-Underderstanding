69f9dbcded77972e7f13b554b88836036b160cda
ShMcK <me@shmck.com>
2015-08-06 11:28:50 +0900

    form setup

--- forms/client/app.ng.ts
+++ forms/client/app.ng.ts
@@ -1,8 +1,9 @@
 import {Component, View, bootstrap} from 'angular2/angular2';
-import {formDirectives, NgControl, Validators, NgForm} from 'angular2/forms';
+import {formDirectives, Control, Validators, NgForm} from 'angular2/forms';
 
 interface IModel {
-  name: string;
+  ngModel?: string;
+  initText: string;
 }
 
 @Component({
@@ -14,11 +15,21 @@ interface IModel {
 })
 class App {
   model:IModel;
+  control1: Control;
+  control2: Control;
+  control3: Control;
 
   constructor() {
     this.model = {
-      name: 'Some Name'
-    }
+      initText: 'Some Text'
+    };
+
+    this.control1 = new Control('');
+    this.control2 = new Control('Init Value');
+    this.control3 = new Control('');
+  }
+  callControl(event) {
+    console.log(event.keyCode);
   }
 }
 
